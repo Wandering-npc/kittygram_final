164a720d952874f67d9257c14f95cc8c319db8ba
Виталий Прокин <Los-Shepardos@yandex.ru>
2023-08-10 00:02:43 +0300

    dasadsdsa

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,13 +5,11 @@ from django.core.management.utils import get_random_secret_key
 
 from dotenv import load_dotenv
 
-get_random_secret_key()
-
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
