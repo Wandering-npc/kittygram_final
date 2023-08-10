c9064e73338cbce32ea17e38e967ceead2e9688a
WorkPC <Los-Shepardos@yandex.ru>
2023-08-10 09:32:57 +0300

    ddasd

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,10 +9,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if 'SECRET_KEY' not in os.environ:
-    raise Exception('Секретный ключ не найден')
-
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
