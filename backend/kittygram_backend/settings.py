d7a68d3b384766e376f2acdd7ad77a2c88b709a7
WorkPC <Los-Shepardos@yandex.ru>
2023-08-10 15:06:49 +0300

    done

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 import os
 from pathlib import Path
-from django.core.management.utils import get_random_secret_key  
 
+from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 load_dotenv()
